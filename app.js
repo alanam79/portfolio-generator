537e013ca421f960ecc30a44fc7451fbbb947501
alanam79 <halo2382001@gmail.com>
2022-05-23 19:22:06 -0400

    added ignore file

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
+const generatePage = (userName, githubName) => 'Name: ${userName}, Github: $(githubName}';
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log("================");
-
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-
-};
-
-printProfileData(profileDataArgs);
+console.log(generatePage('Jane', 'janehub'));
