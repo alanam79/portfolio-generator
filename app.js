0c70dd6d58a2a7552d9e3d8d86a79aa065da8f1f
alanam79 <halo2382001@gmail.com>
2022-05-24 14:20:13 -0400

    added second page to clean up code

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const generatePage = (userName, githubName) => 'Name: ${userName}, Github: $(githubName}';
+const [name, github] = profileDataArgs;
 
-console.log(generatePage('Jane', 'janehub'));
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+    if (err) throw new Error(err);
+  
+    console.log("Portfolio complete! Check out index.html to see the output!");
+  });
+  
+
